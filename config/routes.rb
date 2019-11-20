499358e4eb66e00aef14a092a90f2d3b2a7ac5af
Jeffrey Yeung <jeff.yeung23@gmail.com>
2019-11-20 16:36:12 +0800

    basic functions completed - there is a rake error that cannot be solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/edit'
+  get 'restaurants/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+5.times do
+  r = Restaurant.create!(name: Faker::Restaurant.name,
+                         address: Faker::Address.street_address,
+                         phone_number: Faker::PhoneNumber.phone_number,
+                         category: %w(chinese italian japanese french belgian).sample)
+  puts "Created #{r.name}"
+  Review.create!(content: Faker::TvShows::GameOfThrones.quote,
+                 rating: rand(1..5),
+                 restaurant_id: Restaurant.all.sample.id)
+end
+
+puts "Created #{Restaurant.count} restaurant"
